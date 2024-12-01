209fe6ddc29cd4eb84ca0ab9944077ed4c586439
shehab-kharaz <shehababbaskharaz.222@hotmail.com>
2024-12-01 13:10:37 +0200

    feat: Implement navbar build functionality
    Create and implement buildNavbar function which building the navbar
    dynamically using JavaScript

--- js/app.js
+++ js/app.js
@@ -18,45 +18,104 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
-/**
- * Define Global Variables
- * 
+
+
+
+
+
+
+// Define Global Variables
+
+const navbarList = document.querySelector("#navbar__list")
+const sections = document.querySelectorAll("section")
+
+
+
+
+/*
+  End Global Variables
+  Start Helper Functions
 */
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
+
+
+
+/*
+  End Helper Functions
+  Begin Main Functions 
 */
 
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+
+
 
 // build the nav
+
+
+
+
 
 
 // Add class 'active' to section when near top of viewport
 
 
+
+
+
+
 // Scroll to anchor ID using scrollTO event
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
+
+
+
+
+
+/*
+  End Main Functions
+  Begin Events
 */
 
+
+
+
+
+
+
 // Build menu 
+function buildNavbar(){
+  sections.forEach(section => {
+   
+    const listItem = document.createElement("li");
+    const anchor = document.createElement("a");
+
+    const anchorTarget = section.id;
+    const anchorTitle = section.dataset.nav;
+
+    anchor.textContent = anchorTitle;
+    anchor.href = `#${anchorTarget}`;
+
+    listItem.appendChild(anchor);
+    navbarList.appendChild(listItem);
+
+  })
+}
+
+
+
+
+
+
+
 
 // Scroll to section on link click
 
+
+
+
+
+
+
 // Set sections as active
-
-
