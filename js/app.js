1583cbec0fc57b9849cd11755a6a0c082bfbd4fa
shehab-kharaz <shehababbaskharaz.222@hotmail.com>
2024-12-02 12:37:07 +0200

    docs: Document main functions in the app.js

--- js/app.js
+++ js/app.js
@@ -5,16 +5,22 @@ const sections = document.querySelectorAll("section");
 let scrollingTimeOut;
 
 
+// Helper functions
 
-
-// helper function to call document loaded listeners 
+/**
+ * @description Initializes event listeners when the document is ready.
+ * Builds the navigation bar and sets up an event listener to collapse sections when a click event occurs.
+ */
 function callAllWhenDocumentReady(){
   buildNavbar();
   document.addEventListener("click", event => collapseSections(event));
 }
 
 
-// helper function to call scroll listeners 
+/**
+ * @description Sets up various scroll-related listeners and actions.
+ * It initializes the active section, sets the visibility of the scroll button, and hides the fixed header.
+ */
 function callScrollListeners(){
   setActiveSection();
   setScrollBtnVisibility();
@@ -24,10 +30,12 @@ function callScrollListeners(){
 
 
 
+// Main functions
 
-
-
-// build the navbar
+/**
+ * @description Builds the navigation bar dynamically based on the sections on the page.
+ * Iterates through each section, creating a list item and anchor tag for each, and appends them to the navbar.
+ */
 function buildNavbar(){
   sections.forEach(section => {
    
@@ -48,7 +56,10 @@ function buildNavbar(){
 }
 
 
-// Add class 'active' to section when near top of viewport
+/**
+ * @description Sets the active section and updates the corresponding navigation link.
+ * Iterates through each section and checks if it's in view. If the section is in view, it adds an active class; otherwise, it removes the class.
+ */
 function setActiveSection(){
   sections.forEach(section => {
     const boundingRect = section.getBoundingClientRect();
@@ -66,7 +77,11 @@ function setActiveSection(){
 }
 
 
-// Scroll to anchor ID using scrollTO event
+/**
+ * @description Smoothly scrolls to the target section when a navigation link is clicked.
+ * Prevents the default link behavior, retrieves the target section, and scrolls to it with smooth animation.
+ * @param {Event} event - The click event triggered when a navigation link is clicked.
+ */
 function scrollToSection(event){
   event.preventDefault();
   const targetId = event.target.getAttribute("href");
@@ -78,20 +93,28 @@ function scrollToSection(event){
 }
 
 
-// show header
+/**
+ * @description Displays the header by setting its top position to 0.
+ */
 function showHeader(){
   header.style.top = "0";
 }
 
 
-// hide header
+/**
+ * @description Hides the header by moving it off-screen based on its height.
+ * Retrieves the header's height and sets its top position to a negative value to hide it.
+ */
 function hideHeader(){
   const headerHeight = header.getBoundingClientRect().height;
   header.style.top = `-${headerHeight}px`;
 }
 
 
-// Toggle the visibility of the scroll to top button 
+/**
+ * @description Toggles the visibility of the "scroll to top" button based on the scroll position.
+ * Displays the button if the page is scrolled more than 200 pixels, otherwise hides it.
+ */
 function setScrollBtnVisibility(){
   if(window.scrollY > 200)
     scrollToTopBtn.style.display = "block";
@@ -100,7 +123,9 @@ function setScrollBtnVisibility(){
 }
 
 
-// Scroll to the top when button is clicked
+/**
+ * @description Smoothly scrolls the page to the top when called.
+ */
 function scrollToTop(){
   window.scrollTo({
     top: 0,
@@ -109,7 +134,11 @@ function scrollToTop(){
 }
 
 
-// Hide fixed header when user not scrolling 
+/**
+ * @description Manages the visibility of the fixed header based on scroll position.
+ * Shows the header when the scroll position is less than 10 pixels. If the scroll position is greater, 
+ * it hides the header after a delay of 1200 milliseconds.
+ */
 function hideFixedHeader(){
   if(window.scrollY < 10){
     showHeader();
@@ -123,12 +152,21 @@ function hideFixedHeader(){
 }
 
 
-// Reenable header hiding when not hovered over
+/**
+ * @description Re-enables the header hiding functionality if the scroll position is not less than 10 pixels.
+ * Sets a timeout to hide the header after a 1200 millisecond delay if the scroll position is greater than or equal to 10 pixels.
+ */
 function reenableHeaderHiding(){
   if(!(window.scrollY < 10))
     scrollingTimeOut = setTimeout(hideHeader, 1200)
 }
 
+
+/**
+ * @description Toggles the visibility of a section's content when a toggle button is clicked.
+ * Changes the symbol of the button between "+" and "−" to indicate the section's state (expanded or collapsed).
+ * @param {Event} event - The click event triggered when a toggle button is clicked.
+ */
 
 function collapseSections(event){
   if (event.target && event.target.classList.contains("toggle-section")){
@@ -149,40 +187,17 @@ function collapseSections(event){
 
 
 
-
-
-
 // Events
-
-
-// Build menu 
 document.addEventListener("DOMContentLoaded", callAllWhenDocumentReady)
-
-
-// Set sections as active
 window.addEventListener("scroll", callScrollListeners)
-
-
-// Return to the top of the document
 scrollToTopBtn.addEventListener("click", scrollToTop)
-
-// Reenable header hiding when not hovered over
 header.addEventListener("mouseleave", reenableHeaderHiding)
-
-
-// Header stay visible when hovered
 header.addEventListener("mouseenter", () => clearTimeout(scrollingTimeOut))
-
-
-// Show the header when the mouse near the top of the document 
 document.addEventListener("mousemove", function(event) {
   if (event.clientY <= 15) {
       showHeader(); 
   }
 });
-
-
-// Scroll to section on link click
 navbarList.addEventListener("click", (event) => {
   if(event.target.tagName === "A")
     scrollToSection(event);
